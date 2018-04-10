bbc84605a34ae2c61fcac4b8480a4eff10cdfa94
Tony <diablotony01@hotmail.com>
2018-04-10 01:45:57 -0700

    added all pong files

---
+++ src/partials/Game.js
@@ -0,0 +1,17 @@
+export default class Game {
+
+	constructor(element, width, height) {
+		this.element = element;
+		this.width = width;
+		this.height = height;
+		this.gameElement = document.getElementById(element);
+		
+		
+		// Other code goes here...
+	}
+
+	render() {
+		// More code goes here...
+	}
+
+}
